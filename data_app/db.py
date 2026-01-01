b716b6c4c77282bad54047100c00ca5cf2e1013b
Rafael Trindade <rafatrindade.exe@gmail.com>
2025-12-31 23:41:32 -0300

    feat(streamlit): adiciona data app streamlit

--- data_app/db.py
+++ data_app/db.py
@@ -3,10 +3,8 @@ from dotenv import load_dotenv
 import os
 from pathlib import Path
 
-# Caminho da raiz do projeto
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
-# Carrega .env da raiz
 load_dotenv(PROJECT_ROOT / ".env")
 
 def get_engine():
